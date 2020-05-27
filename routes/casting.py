e122e2b6471424f20bd25da0393c8a6f7dc302e4
Eliasar Gandara <eliasargandara@gmail.com>
2020-05-27 01:45:51 -0700

    update actor route

--- routes/casting.py
+++ routes/casting.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from werkzeug.exceptions import BadRequest
+from werkzeug.exceptions import BadRequest, NotFound
 from marshmallow import ValidationError
 from sqlalchemy.orm import load_only
 
@@ -55,8 +55,58 @@ def create_actor(token):
         movies=movies
     )
     actor.insert()
+    serialized = schema.dump(actor)
 
     return jsonify({
         'success': True,
-        'id': actor.id
+        'data': [serialized]
+    })
+
+
+@casting_blueprint.route('/actors/<actor_id>', methods=['PATCH'])
+@requires_auth('patch:actors')
+def update_actor(actor_id, token):
+    if not request.data:
+        raise BadRequest(
+            description='The request did not contain a request body.'
+        )
+
+    if not request.is_json:
+        raise BadRequest(
+            description='The request expected a JSON request body.'
+        )
+
+    schema = ActorSchema()
+    data = schema.load(request.get_json(), partial=True)
+
+    actor = Actor.query.\
+        filter(Actor.id == actor_id).\
+        one_or_none()
+
+    if not actor:
+        raise NotFound(
+            description=f'An actor with the id "{actor_id}" was not found.'
+        )
+
+    if 'name' in data:
+        actor.name = data['name']
+    if 'age' in data:
+        actor.age = data['age']
+    if 'gender' in data:
+        actor.gender = data['gender']
+    if 'movie_ids' in data:
+        movie_ids = data['movie_ids']
+        movies = Movie.query.\
+            filter(Movie.id.in_(movie_ids)).\
+            options(load_only('id')).\
+            all()
+
+        actor.movies = movies
+
+    actor.update()
+    serialized = [schema.dump(actor)]
+
+    return jsonify({
+        'success': True,
+        'data': serialized
     })
